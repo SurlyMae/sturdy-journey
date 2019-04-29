6d3878e68bd62435d29c54a8e02d19b95c1fa601
Ashly LaMarr <surlymae@gmail.com>
2019-04-29 12:14:51 -0600

    Set up main entry point

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Net;
+using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
@@ -8,6 +9,15 @@ namespace server
 {
     class Program
     {
+        static TcpListener listener;
+        static Thread serverThread;
 
+        static Dictionary<int, State> connections = new Dictionary<int, State>();
+        public static void Main (string[] args)
+        {
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+            serverThread = new Thread(new ThreadStart(Listen));
+            serverThread.Start();
+        }
     }
 }
